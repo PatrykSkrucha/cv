20e4dcdc4b33ba97ea6cbd0c9d56669728354594
PatrykSkrucha <patryk.skrucha@gmail.com>
2019-01-16 13:47:52 +0100

    bfly

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import Page from './containers/Page/Page'
-import Nav from './containers/Navbar/Navbar'
 
 class App extends Component {
 	
 	
 	render() {
 		return (
-			<>
-				
-				
-				
+			<>				
 				<Page />
 			</>
 		);
