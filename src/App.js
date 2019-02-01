f8f760d09b8ea8fb1f0bdc89f1e9949f320e3f1a
PatrykSkrucha <patryk.skrucha@gmail.com>
2019-02-01 16:17:17 +0100

    working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
 import Page from './containers/Page/Page'
-import { ParallaxProvider } from 'react-scroll-parallax';
-
 
 class App extends Component {
 	
-	scrolling = () => {
-		console.log(`scrolling`);
-		
-	}
 	render() {
 		return (
 			<>	
-				<ParallaxProvider>
-					<Page />
-				</ParallaxProvider>			
+				<Page />
 			</>
 		);
 	}
