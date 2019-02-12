2c2d44edd9eadf63b9e58dec8ffc7b61aeebb8a3
PatrykSkrucha <patryk.skrucha@gmail.com>
2019-02-12 16:41:31 +0100

    spring library

--- src/containers/Intro/Intro.js
+++ src/containers/Intro/Intro.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import classes from './Intro.scss';
+import { useSpring, useChain, animated } from 'react-spring'
 
-const intro = () => (
-	<div className={classes.Intro}>
+const intro = () => {
+	const contRef = useRef()
+	const container = useSpring({
+		from: {opacity: 0},
+		to: {opacity: 1},
+		delay: 500,
+		// duration: 5000
+	})
+	
+	const opacityRef = useRef();
+	const opacity = useSpring({
+		from: { opacity: 0 },
+		to: { opacity: 1 },
+		// delay: 1000,
+		ref: opacityRef
+	})
+	const paddingRef = useRef();
+	const padding = useSpring({
+		from: {color: 'white'},
+		to: {color: 'cadetblue'},
+		// delay: 3000,
+		ref: paddingRef
+	})
+	const chain =  useChain([opacityRef, paddingRef], [1,2],)
+	
+	
+	return (
+		<div  className={classes.Intro}>
 
-	<div className={classes.Container}>
-		<span className={classes.Text1}>Welcome</span>
+	<animated.div style={container} className={classes.Container}>
+		<animated.span  className={classes.Text1} style={opacity}>Welcome</animated.span>
 		<span className={classes.Text2}> on my site</span>
-	</div>
+	</animated.div>
 		
 	</div>
 )
+} 
 
 export default intro;
--- src/containers/Photo/Photo.js
+++ src/containers/Photo/Photo.js
@@ -6,17 +6,15 @@ import handleViewport from 'react-in-viewport';
 const photo = (props: { inViewport: boolean }) => {
 
 	const { innerRef } = props;
-	const translate = props.loaded ? 'rotateX(0) translateX(0)' : 'rotateX(180deg) translateX(-100vh)';
-	const opacity = props.loaded ? '1' : '0'
+	const translate = props.loaded ? 'translateX(0)' : 'translateX(-100vh)';
 
 	
 	return (
 
 	<div className={classes.Photo} ref={props.innerRef}
 	style={{
-		opacity: opacity,
-		//{/* transform: translate, */}
-		transition: 'opacity 0.6s',
+		transform: translate,
+		transition: 'transform 0.6s',
 	}}>
 		<img src={pic} alt="a picture of me" />
 	</div>
