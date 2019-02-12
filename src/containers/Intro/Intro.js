7a1d45bc02d8487b0b1bc5639560a41afafb8cdc
PatrykSkrucha <patryk.skrucha@gmail.com>
2019-02-12 18:00:32 +0100

    animations intro

--- src/containers/Intro/Intro.js
+++ src/containers/Intro/Intro.js
@@ -3,37 +3,45 @@ import classes from './Intro.scss';
 import { useSpring, useChain, animated } from 'react-spring'
 
 const intro = () => {
-	const contRef = useRef()
+
+	const containerRef = useRef()
 	const container = useSpring({
 		from: {opacity: 0},
 		to: {opacity: 1},
 		delay: 500,
-		// duration: 5000
+		ref: containerRef,
 	})
 	
-	const opacityRef = useRef();
-	const opacity = useSpring({
-		from: { opacity: 0 },
-		to: { opacity: 1 },
-		// delay: 1000,
-		ref: opacityRef
+	const txt1Ref = useRef();
+	const txt1 = useSpring({
+		from: {opacity: 0, color: 'white', 'padding-top': '40px'
+	},
+		to: async next => {
+			await next ({opacity: 1})
+			await next ({'padding-top': '5px'})
+			
+		},
+		ref: txt1Ref
+
 	})
-	const paddingRef = useRef();
-	const padding = useSpring({
-		from: {color: 'white'},
-		to: {color: 'cadetblue'},
-		// delay: 3000,
-		ref: paddingRef
+	
+	const txt2Ref = useRef();
+	const txt2= useSpring({
+		from: {opacity: 0, 'padding-top': '0px'},
+		to: { opacity: 1, 'padding-top': '20px'},
+		ref: txt2Ref
 	})
-	const chain =  useChain([opacityRef, paddingRef], [1,2],)
+
+
+	const chain =  useChain([containerRef, txt1Ref, txt2Ref], [0.2,1, 3],)
 	
 	
 	return (
 		<div  className={classes.Intro}>
 
 	<animated.div style={container} className={classes.Container}>
-		<animated.span  className={classes.Text1} style={opacity}>Welcome</animated.span>
-		<span className={classes.Text2}> on my site</span>
+		<animated.span style={txt1} className={classes.Text1} >Welcome</animated.span>
+		<animated.span style={txt2} className={classes.Text2}> on my site</animated.span>
 	</animated.div>
 		
 	</div>
